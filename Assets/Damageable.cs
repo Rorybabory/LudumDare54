4282e04bec4620bc8d4e7c51f4a299fee407e494
Travis French <travis.french.1991@gmail.com>
2023-09-29 23:33:56 -0700

    Created damageable component.

---
+++ Assets/Damageable.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    [Serializable]
+    public class CounterData
+    {
+        public int Value = 3;
+        public int Maximum = 3;
+    }
+
+    [SerializeField]
+    private CounterData counter;
+    [Header("Event Callbacks")]
+    [SerializeField]
+    private UnityEvent damaged;
+
+    public CounterData Counter => this.counter;
+
+    public event Action Damaged;
+
+    public void TakeDamage(int amount)
+    {
+        var valueAfterDamage = Mathf.Clamp(this.Counter.Value - amount, 0, this.Counter.Maximum);
+
+        this.Counter.Value = valueAfterDamage;
+        
+        if (valueAfterDamage <= 0)
+        {
+            var killable = this.GetComponent<Killable>();
+
+            if (killable != null)
+            {
+                killable.Kill();
+            }
+        }
+    }
+}
