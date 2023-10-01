f7cae3534613324563bc61c589a1dfe903d462ef
Travis French <travis.french.1991@gmail.com>
2023-10-01 16:40:44 -0700

    Created basic radial melee hitscanner.

--- Assets/Attack/MeleeHitScanner.cs
+++ Assets/Attack/MeleeHitScanner.cs
@@ -5,10 +5,51 @@ using UnityEngine;
 
 public class MeleeHitScanner : HitScan
 {
+    [SerializeField]
+    private Transform origin;
+    [SerializeField]
+    private float radius = 3f;
+    [SerializeField]
+    private float angle = 80f;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private QueryTriggerInteraction triggerInteraction = QueryTriggerInteraction.Ignore;
+
+    public Transform Origin
+    {
+        get => this.origin;
+        set => this.origin = value;
+    }
+
+    public float Radius
+    {
+        get => this.radius;
+        set => this.radius = value;
+    }
+
+    public float Angle
+    {
+        get => this.angle;
+        set => this.angle = value;
+    }
+
+    public LayerMask LayerMask
+    {
+        get => this.layerMask;
+        set => this.layerMask = value;
+    }
+    public QueryTriggerInteraction TriggerInteraction
+    {
+        get => this.triggerInteraction;
+        set => this.triggerInteraction = value;
+    }
+
     public override Collider[] ScanForColliders()
     {
-        //return melee collider array
-        throw new NotImplementedException();
-
+        var collidersInRadius =
+            Physics.OverlapSphere(this.Origin.position, this.Radius, this.LayerMask, this.TriggerInteraction);
+        
+        return collidersInRadius;
     }
 }
---
+++ Assets/Editor/MeleeHitScannerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MeleeHitScanner))]
+public class MeleeHitScannerEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        var scanner = (MeleeHitScanner)this.target;
+        var baseColor = Color.yellow;
+        var color = new Color()
+        {
+            r = baseColor.r,
+            g = baseColor.g,
+            b = baseColor.b,
+            a = 0.5f,
+        };
+
+        var origin = scanner.Origin == null ? scanner.transform : scanner.Origin;
+        
+        Handles.color = color;
+        Handles.DrawSolidArc(origin.position, origin.up, origin.forward, scanner.Angle, scanner.Radius);
+    }
+}
