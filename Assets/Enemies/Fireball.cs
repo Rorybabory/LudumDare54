fe5ac5156eb62168de06f9d5939b37819b2aa109
Rorybabory <lambertrory123@gmail.com>
2023-10-01 11:22:12 -0700

    Skulls shoot fireballs

--- Assets/Enemies/Fireball.cs
+++ Assets/Enemies/Fireball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private float timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class Fireball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        transform.position += speed * transform.forward * Time.deltaTime;
+        if (timer > 4.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Capsule")
+        {
+            Debug.Log("HIT PLAYER");
+        }
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/SkullEnemy.cs
+++ Assets/Scripts/SkullEnemy.cs
@@ -7,22 +7,35 @@ public class SkullEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private GameObject fireball;
+    [SerializeField] private GameObject fireballPrefab;
     private Vector3 basePos;
 
     private float timetomove;
     private float movetimer;
 
+    private float shoottimer;
+    private float timetoshoot;
+
     private Vector3 offsetvelocity;
     private Vector3 offset;
+
+
     void Start()
     {
         basePos = transform.position;
         timetomove = Random.Range(0f, 1f);
         offsetvelocity = new Vector3(0, 0, 0);
         movetimer = 0;
+        timetoshoot = Random.Range(0.5f, 4.0f);
+        shoottimer = 0;
     }
-
+    void ShootFireball()
+    {
+        GameObject fireball = Instantiate(fireballPrefab, transform);
+        fireball.transform.parent = null;
+        fireball.transform.position = transform.position + new Vector3(0, -0.75f, 0.0f);
+        Debug.Log("Shoot Fireball");
+    }
     // Update is called once per frame
     void Update()
     {
@@ -40,6 +53,13 @@ public class SkullEnemy : MonoBehaviour
             timetomove = Random.Range(1f, 3f);
             movetimer = 0;
             offsetvelocity = new Vector3(Random.Range(-40f, 10f), Random.Range(-3.8f, 3.8f), Random.Range(-40f, 20f));
+        }
+        shoottimer += Time.deltaTime;
+        if (shoottimer > timetoshoot)
+        {
+            ShootFireball();
+            shoottimer = 0;
+            timetoshoot = Random.Range(0.5f, 4.0f);
         }
         offset += offsetvelocity * Time.deltaTime;
     }
