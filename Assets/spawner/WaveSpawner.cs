2191517498f6f1db06a1407aeccc3e33c06435d0
Brevillo <oliver.beebe@digipen.edu>
2023-10-02 14:38:34 -0700

    added wave beaten voice lines

--- Assets/spawner/WaveSpawner.cs
+++ Assets/spawner/WaveSpawner.cs
@@ -12,6 +12,7 @@ public class WaveSpawner : MonoBehaviour {
     [SerializeField] private WaveObject[] waves;
     [SerializeField] private Transform[] spawnpoints;
     [SerializeField] private Transform[] spawnpointsSkull;
+    [SerializeField] private SoundEffect waveBeatenSound;
 
     [SerializeField] private float waveDuration;
 
@@ -21,28 +22,31 @@ public class WaveSpawner : MonoBehaviour {
 
     private float waveTimer = Mathf.Infinity;
     private int points;
-    List<GameObject> spawnedEnemies;
-    void Start()
-    {
+    private List<GameObject> spawnedEnemies;
 
-        newWave();
-    }
-    void newWave()
+    private void Start()
     {
+        waveBeatenSound.Init(gameObject);
+
+        NewWave();
+    }
+
+    private void NewWave()
+    {
+        waveBeatenSound.Play();
+
         spawnedEnemies = waves[currentWave].Spawn(spawnpoints, spawnpointsSkull);
 
         currentWave++;
+        if (currentWave >= waves.Length) currentWave = 0;
+
         waveTimer = 0;
         this.Spawned?.Invoke(spawnedEnemies.Count());
     }
-    private void Update() {
-        Debug.Log("Wave count: " + spawnedEnemies.Count());
 
-        if (spawnedEnemies.Count() <= 0)
-        {
-            Debug.Log("ALL ENEMIES KILLED!!!!!!");
-            newWave();
-        }
+    private void Update() {
+
+        if (spawnedEnemies.Count() <= 0) NewWave();
 
         spawnedEnemies.RemoveAll(e => e == null);
 
