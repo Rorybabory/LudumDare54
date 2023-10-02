7d981f24f3d8dfa356c1393df838a96fb3c3a445
GeriGrape9 <gabrielxplr@outlook.com>
2023-10-02 13:58:55 -0700

    blood effect bettered
    fires souls that empower the gun

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -8,7 +8,7 @@ public class LookAtPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        target = GameObject.FindGameObjectWithTag("Gun").transform;
     }
 
     // Update is called once per frame
